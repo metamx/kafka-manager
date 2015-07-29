01005a68135e90dcfe6636c3d7298a20ffe1a0d0
jisookim <jisookim0513@gmail.com>
2015-07-29 15:47:15 -0700

    sort broker partitions and add a DataTable plugin for broker view table

--- app/kafka/manager/ActorModel.scala
+++ app/kafka/manager/ActorModel.scala
@@ -223,7 +223,7 @@ object ActorModel {
 
       brokerPartitionsMap.map {
         case (brokerId, brokerPartitions)=>
-          BrokerTopicPartitions(brokerId, brokerPartitions.toIndexedSeq,
+          BrokerTopicPartitions(brokerId, brokerPartitions.toIndexedSeq.sorted,
             brokerPartitions.size > avgPartitionsPerBroker)
       }.toIndexedSeq.sortBy(_.id)
     }
